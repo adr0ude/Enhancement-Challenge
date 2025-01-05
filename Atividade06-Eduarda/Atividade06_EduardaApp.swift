7604b61c14524bd3d64bfc52aee007b5850382c5
carolrolis <anacarolinasf0101@gmail.com>
2025-01-05 17:57:43 -0300

    Initial Commit

---
+++ Atividade06-Eduarda/Atividade06_EduardaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atividade06_EduardaApp.swift
+//  Atividade06-Eduarda
+//
+//  Created by iredefbmac_28 on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Atividade06_EduardaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atividade06-Eduarda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Atividade06-Eduarda
+//
+//  Created by iredefbmac_28 on 05/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
